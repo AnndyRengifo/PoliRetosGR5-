0847af33f753db66f6d5db09b02be49f6f3609bc
Joe-575 <medinajoel814@gmail.com>
2024-11-04 10:34:29 -0500

    Update menu

--- poliRetosmio/src/Menu/menu.java
+++ poliRetosmio/src/Menu/menu.java
@@ -1,7 +1,10 @@
 package Menu;
 import SeriesLoading.seriesLoading;
+import SeriesRecursion.seriesRecursion;
 import cadenaCaracteres.CadenaDeCaracteres;
 import java.util.Scanner;
+
+import GrafosyAutomatas.grafosYautomatas;
 import seriesArrays.seriesArrays;
 import seriesCaracteres.SeriesCaracteres;
 import seriesFiguras.SeriesFiguras;
@@ -9,87 +12,71 @@ import seriesNumericas.SeriesNumericas;
 
 public class menu {
 
-    public void mostrarIntegrantes(){
-        System.out.println();
-        System.out.println("\t\t |---------------------------------------------------|"); 
-        System.out.println("\t\t |                     GRUPO N° 5                    |"); 
-        System.out.println("\t\t |---------------------------------------------------|"); 
-        System.out.println("\t\t |                   RENGIFO ANNDY                   |"); 
-        System.out.println("\t\t |                   SANCHEZ ANDY                    |"); 
-        System.out.println("\t\t |                   VALLEJO ALEXIS                  |"); 
-        System.out.println("\t\t |                   VELEZ JOEL                      |"); 
-        System.out.println("\t\t |---------------------------------------------------|"); 
-        System.out.println(" ");
-    }
-    
-    
     public void mostrarMenu() {
-        Scanner sc = new Scanner(System.in);
-        int opcion = 0;
-        
-        do {
-            System.out.println("");
-            System.out.println("\t BIENVENIDO A LOS POLIRETOS");
-            System.out.println("\t  1. Series numericas");
-            System.out.println("\t  2. Serie de caracteres");
-            System.out.println("\t  3. Serie de figuras");
-            System.out.println("\t  4. Cadena de Caracteres");
-            System.out.println("\t  5. Arrays");
-            System.out.println("\t  6. Loading");
-            System.out.println("\t  7. Recursion");
-            System.out.println("\t  8. Grafos y Automatas");
-            System.out.println("\t  9. Salir");
-            System.out.print("\t Seleccione una opcion: ");
-            
-            if (sc.hasNextInt()) {
-                opcion = sc.nextInt();
-                sc.nextLine();
-                switch (opcion) {
-                    case 1:
-                        SeriesNumericas series = new SeriesNumericas();
-                        series.generarSeries(sc); 
-                        break;
-                    case 2:
-                        SeriesCaracteres seriesCaracteres = new SeriesCaracteres();
-                        seriesCaracteres.generarSeries(sc);
-                        break;
-                    case 3:
-                       SeriesFiguras seriesFiguras = new SeriesFiguras();
-                       seriesFiguras.generarFiguras(sc);
-                        break;
-                    case 4:
-                        CadenaDeCaracteres cadena = new CadenaDeCaracteres();
-                        cadena.generarCaracteres(sc);
-                        break;
-                    case 5:
-                        break;
-                    case 6:
-
-                        break;
-                    case 7:
-                        seriesRecursion  recursion = new seriesRecursion();
-                        recursion.generarRecursion(sc);
-                        break;
-
-                    case 8:
-                        grafosYautomatas grafosAutomata = new grafosYautomatas();
-                        grafosAutomata.GR5_generarGrafosyAutomatas(sc);
-                        break;
-
-                    case 9:
-                        System.out.println("\n\t Saliendo del programa");
-                        break;
-                    default:
-                        System.out.println("\t Opcion no valida, ingrese una opcion valida");
+        try (Scanner sc = new Scanner(System.in)) { 
+            int opcion = 0;
+    
+            do {
+                System.out.println("");
+                System.out.println("\t BIENVENIDO A LOS POLIRETOS");
+                System.out.println("\t  1. Series numericas");
+                System.out.println("\t  2. Serie de caracteres");
+                System.out.println("\t  3. Serie de figuras");
+                System.out.println("\t  4. Cadena de Caracteres");
+                System.out.println("\t  5. Arrays");
+                System.out.println("\t  6. Loading");
+                System.out.println("\t  7. Recursion");
+                System.out.println("\t  8. Grafos y Automatas");
+                System.out.println("\t  9. Salir");
+                System.out.print("\t Seleccione una opcion: ");
+    
+                if (sc.hasNextInt()) {
+                    opcion = sc.nextInt();
+                    sc.nextLine();
+                    switch (opcion) {
+                        case 1 -> {
+                            SeriesNumericas series = new SeriesNumericas();
+                            series.generarSeries(sc);
+                        }
+                        case 2 -> {
+                            SeriesCaracteres seriesCaracteres = new SeriesCaracteres();
+                            seriesCaracteres.generarSeries(sc);
+                        }
+                        case 3 -> {
+                            SeriesFiguras seriesFiguras = new SeriesFiguras();
+                            seriesFiguras.generarFiguras(sc);
+                        }
+                        case 4 -> {
+                            CadenaDeCaracteres cadena = new CadenaDeCaracteres();
+                            cadena.generarCaracteres(sc);
+                        }
+                        case 5 -> {
+                            seriesArrays arrays = new seriesArrays();
+                            arrays.generarArrays(sc);
+                        }
+                        case 6 -> {
+                            seriesLoading loading = new seriesLoading();
+                            loading.generarLoading(sc);
+                        }
+                        case 7 -> {
+                            seriesRecursion recursion = new seriesRecursion();
+                            recursion.generarRecursion(sc);
+                        }
+                        case 8 -> {
+                            grafosYautomatas grafosAutomata = new grafosYautomatas();
+                            grafosAutomata.GR5_generarGrafosyAutomatas(sc);
+                        }
+                        case 9 -> System.out.println("\n\t Saliendo del programa");
+                        default -> System.out.println("\t Opción no válida, ingrese una opción válida");
+                    }
+                } else {
+                    System.out.println("\t Opción no válida, ingrese una opción válida");
+                    sc.next(); // Consumir el valor no entero ingresado
                 }
-            } else {
-                System.out.println("\t Opcion no valida, ingrese una opcion valida");
-                sc.next(); 
-            }
-            System.out.println("\n");
-        } while (opcion != 9);
-
-        sc.close();
+                System.out.println("\n");
+            } while (opcion != 9);
+    
+        }
     }
 }
 
